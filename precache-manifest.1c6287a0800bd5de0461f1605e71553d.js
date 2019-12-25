fa2d35526c491890dac2d374ff5bdd6ba40f2de6
linetyreborn <o.hedda@gmail.com>
2019-12-25 20:12:42 +0100

    Updates

---
+++ precache-manifest.1c6287a0800bd5de0461f1605e71553d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93fd0ebcc8127556f957381c07a7129",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9dd31fea470eaf7cb2b2",
+    "url": "/monsters-rolodex/static/css/main.f6a1d3f8.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9dd31fea470eaf7cb2b2",
+    "url": "/monsters-rolodex/static/js/main.187773b1.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
